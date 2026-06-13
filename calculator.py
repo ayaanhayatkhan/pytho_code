5227ca9965bbeea381f0f76a4fbe3689c9ae64a4
AyaanHayatKhan <ayaanhayatkhan009@gmail.com>
2026-06-13 23:48:56 +0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# Addition with int
+
+from cs50 import get_int 
+
+# Prompt user for x
+x = input("x: ")
+
+# Prompt user for y
+y = input("y: ")
+
+# Prompt addition
+print(int(x) +int(y))
+
+
+#source cs50venv/bin/activate
---
+++ list.py
@@ -0,0 +1,14 @@
+mix = [1,"Hello", 5.5,]
+print (mix)
+
+print (mix[0] + 1)
+print (mix[1] + " ,World")
+print(mix[2]* 2)
+print(mix[-1]* 2)
+print (mix[0:3])
+
+mix.append("Ayaan")
+print (mix)
+
+mix.remove("Ayaan")
+print (mix)
